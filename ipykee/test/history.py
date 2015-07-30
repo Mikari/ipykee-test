8487c0e971ff6024dd20688e017a52359b57ca69
ipykee <ipykee@fake.email>
2015-07-30 08:49:17 +0000

    test: BC

--- ipykee/test/history.py
+++ ipykee/test/history.py
@@ -6,8 +6,7 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "%%javascript\n",
-    "IPython.load_extensions('ipykee')"
+    "print \"543\""
    ]
   },
   {
@@ -16,42 +15,8 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "import ipykee\n",
-    "ipykee.create_project_if_not_exist(\"Test\", repository=\"https://github.com/Mikari/ipykee-test\", internal_path=\"ipykee\")\n",
-    "session = ipykee.Session(notebook=\"test\", project_name=\"Test\", docker_image=\"anaderi/rep-develop:latest\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import ipykee\n",
-    "delete_project(\"Test\")\n",
-    "ipykee.create_project_if_not_exist(\"Test\", repository=\"git@github.com:Mikari/ipykee-test.git\", internal_path=\"ipykee\")\n",
-    "session = ipykee.Session(notebook=\"test\", project_name=\"Test\", docker_image=\"anaderi/rep-develop:latest\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import ipykee\n",
-    "ipykee.delete_project(\"Test\")\n",
-    "ipykee.create_project_if_not_exist(\"Test\", repository=\"git@github.com:Mikari/ipykee-test.git\", internal_path=\"ipykee\")\n",
-    "session = ipykee.Session(notebook=\"test\", project_name=\"Test\", docker_image=\"anaderi/rep-develop:latest\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "print \"123\""
+    "%%javascript\n",
+    "IPython.load_extensions('ipykee')"
    ]
   }
  ],
--- ipykee/test/history_manual.py
+++ ipykee/test/history_manual.py
@@ -3,33 +3,15 @@
 
 # <codecell>
 
+print "543"
+
+# <codecell>
+
 %%javascript
 IPython.load_extensions('ipykee')
 
 # <codecell>
 
 import ipykee
-ipykee.create_project_if_not_exist("Test", repository="https://github.com/Mikari/ipykee-test", internal_path="ipykee")
-session = ipykee.Session(notebook="test", project_name="Test", docker_image="anaderi/rep-develop:latest")
-
-# <codecell>
-
-import ipykee
-delete_project("Test")
 ipykee.create_project_if_not_exist("Test", repository="git@github.com:Mikari/ipykee-test.git", internal_path="ipykee")
 session = ipykee.Session(notebook="test", project_name="Test", docker_image="anaderi/rep-develop:latest")
-
-# <codecell>
-
-import ipykee
-ipykee.delete_project("Test")
-ipykee.create_project_if_not_exist("Test", repository="git@github.com:Mikari/ipykee-test.git", internal_path="ipykee")
-session = ipykee.Session(notebook="test", project_name="Test", docker_image="anaderi/rep-develop:latest")
-
-# <codecell>
-
-print "123"
-
-# <codecell>
-
-print "654"
